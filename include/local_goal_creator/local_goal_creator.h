74b5dc7d6aa64c431436377b68b6c875ed3a2cfd
Toshiki Nakamura <tnakamura.amsl@gmail.com>
2024-05-04 22:23:29 +0900

    Add comments

--- include/local_goal_creator/local_goal_creator.h
+++ include/local_goal_creator/local_goal_creator.h
@@ -15,18 +15,64 @@
 #include <ros/ros.h>
 #include <tf2/LinearMath/Quaternion.h>
 
+/**
+ * @class LocalGoalCreator
+ * @brief Local goal creator class
+ */
 class LocalGoalCreator
 {
 public:
+  /**
+   * @brief Construct a new Local Goal Creator object
+   */
   LocalGoalCreator();
+
+  /**
+   * @brief Process local goal creation
+   */
   void process();
 
 private:
+  /**
+   * @brief Path callback
+   *
+   * @param msg Path message
+   */
   void path_callback(const nav_msgs::Path::ConstPtr &msg);
+
+  /**
+   * @brief Pose callback
+   *
+   * @param msg Pose message
+   */
   void pose_callback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &msg);
+
+  /**
+   * @brief Create goal
+   *
+   * @param robot_pose Robot pose
+   * @param path Path
+   * @return geometry_msgs::PoseStamped Goal
+   */
   geometry_msgs::PoseStamped
   create_goal(const geometry_msgs::PoseWithCovarianceStamped &robot_pose, const nav_msgs::Path &path);
+
+  /**
+   * @brief Calculate distance between two points
+   *
+   * @param point1 Point1
+   * @param point2 Point2
+   * @return float Distance between two points
+   */
   float calc_dist_between_points(const geometry_msgs::Point &point1, const geometry_msgs::Point &point2);
+
+  /**
+   * @brief Calculate direction between two points
+   *
+   * @param point1 Point1
+   * @param point2 Point2
+   * @return geometry_msgs::Quaternion Direction between two points
+   */
   geometry_msgs::Quaternion calc_direction(const geometry_msgs::Point &point1, const geometry_msgs::Point &point2);
 
   int hz_;
